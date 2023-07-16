c7ae78a2965b9ff2f775aaa9e4ec6deca8eb5c7f
Rafael3Liu <305055411@qq.com>
2023-07-16 17:51:40 +1200

    sideBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import {BrowserRouter,Router,Route} from 'react-router-dom';
-import {Box} from '@mui/material';
-import {Navbar,Feed,ChannelDetail,VideoDetail,SearchFeed} from './components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Navbar, Feed, ChannelDetail, VideoDetail, SearchFeed } from './components'
 
 
-const App = () => {
-  
-    <BrowserRouter>
-      <Box sx={{ backgroundColor:'#000'}}>
-        <Navbar  />
-        <Router>
-            <Route path="/" exact element={<Feed />} />
-            <Route path="/video/:id" element={<VideoDetail />} />
-            <Route path="/channel/:id" element={<ChannelDetail />} />
-            <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        </Router>
-      </Box>
-    </BrowserRouter>
- 
-}
+const App = () => (
+
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+
+)
 
 export default App
